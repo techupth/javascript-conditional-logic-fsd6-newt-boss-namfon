7aacd920e184fd55ee714e17e0d999eac83b316f
paphonwat <paphonwat.w@gmail.com>
2024-05-15 14:12:45 +0700

    feat:add ex1-ex3

--- ex-2.js
+++ ex-2.js
@@ -2,3 +2,13 @@
 let lightBulbStatus = "On";
 
 // Start coding here.
+lightBulbStatus = "Broken"
+
+if (lightBulbStatus === "on")  {
+    console.log("Light bulb is On.");
+}else if (lightBulbStatus === "Broken") {
+    console.log("Light bulb is Broken.");
+}else{
+    console.log("“Please choose the correct input (On/Off/Broken)”");
+};
+
--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,7 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+message = (lightBulbStatus === "on") ? "Light bulb is On."
+            : (lightBulbStatus === "Broken") ? "Light bulb is Broken."
+            : "“Please choose the correct input (On/Off/Broken)”";
+console.log(message);
