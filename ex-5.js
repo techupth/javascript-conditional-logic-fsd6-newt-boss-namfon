f988b3b4f256030ce565be973f554fb0730b120b
paphonwat <paphonwat.w@gmail.com>
2024-05-15 15:01:44 +0700

    feat:add ex4-6

--- ex-5.js
+++ ex-5.js
@@ -1,6 +1,19 @@
 function calculateStudentGrade(score) {
   // Start coding here
+  if (score >= 0 && score <= 59) {
+    return "F";
+  } else if (score >= 60 && score <= 69) {
+    return "D";
+  } else if (score >= 70 && score <= 79) {
+    return "C";
+  } else if (score >= 80 && score <= 89) {
+    return "B";
+  } else {
+    return "A";
+  }
 }
+
+
 
 // Example case
 let johnGrade = calculateStudentGrade(30);
@@ -14,3 +27,6 @@ console.log(`Lisa grade is ${lisaGrade}`);
 console.log(`Jane grade is ${janeGrade}`);
 console.log(`JJ grade is ${jjGrade}`);
 console.log(`Alex grade is ${alexGrade}`);
+
+
+
